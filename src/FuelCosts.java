41f528397feab44d29d8c40ffbe71069344fde99
mmaalin <maalin.622139@swl.k12.oh.us>
2024-10-02 08:47:19 -0400

    still need to do the final output

--- src/FuelCosts.java
+++ src/FuelCosts.java
@@ -11,33 +11,54 @@ public class FuelCosts {
         boolean allInputs = false;
 
         do {
-            for (int i = 0; i < 3; i++) {
-                scan.nextLine();
+
+            for (int i = 1; i <= 3; i++) {
                 if (i == 1) {
                     System.out.println("Please input the number of gallons you hold in your gas tank.");
-                    if (scan.hasNextDouble() && scan.nextDouble() >= 0) {
+                    if (scan.hasNextDouble()) {
                         gallons = scan.nextDouble();
+                        if (gallons < 0) {
+                            System.out.println("You have inputted an invalid response. Please try again.");
+                            i--;
+                        }
                     } else {
                         System.out.println("You have inputted an invalid response. Please try again.");
+                        i--;
                     }
+                    scan.nextLine();
                 } else if (i == 2) {
                     System.out.println("Please input your car's fuel efficiency in miles per gallon.");
-                    if (scan.hasNextDouble() && scan.nextDouble() > 0) {
+                    if (scan.hasNextDouble()) {
                         fuelEfficiencyMilesPerGal = scan.nextDouble();
+                        if (fuelEfficiencyMilesPerGal <= 0) {
+                            System.out.println("You have inputted an invalid response. Please try again.");
+                            i--;
+                        }
                     } else {
                         System.out.println("You have inputted an invalid response. Please try again.");
+                        i--;
                     }
-                } else {
+                    scan.nextLine();
+                } else if (i==3) {
                     System.out.println("Please input the price per gallon of gas.");
-                    if (scan.hasNextDouble() && scan.nextDouble() > 0) {
+                    if (scan.hasNextDouble()) {
                         gasPricePerGal = scan.nextDouble();
+                        if (gasPricePerGal <= 0) {
+                            System.out.println("You have inputted an invalid response. Please try again.");
+                            i--;
+                        }
                     } else {
                         System.out.println("You have inputted an invalid response. Please try again.");
+                        i--;
                     }
+                    scan.nextLine();
                 }
+                allInputs = true;
             }
-            allInputs = true;
         } while (!allInputs);
+
+        /* put here the calculator for the cost per 100 miles in dolarras
+        put here the distance left in miles */
 
     }
 }
